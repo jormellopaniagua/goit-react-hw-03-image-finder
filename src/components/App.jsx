7b593ca8eca7f195275b984139e7fa3c7bffb399
jormellopaniagua <jormellopaniagua@gmail.com>
2024-05-03 21:36:28 -0500

    aun falta

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,97 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import axios from 'axios';
+import Searchbar from './Searchbar';
+import ImageGallery from './ImageGallery';
+import Button from './Button';
+import Loader from './Loader';
+import Modal from './Modal';
+
+const API_KEY = '43149026-ef77b7f6113923fd46a63d2ce';
+export class App extends Component {
+  state = {
+    images: [],
+    isLoading: false,
+    error: null,
+    query: '',
+    page: 1,
+    showModal: false,
+    selectedImage: null,
+  };
+
+  componentDidMount() {
+    this.fetchImages();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+      this.fetchImages();
+    }
+  }
+
+  componentWillUnmount() {
+    // Cleanup or unsubscribe from any subscriptions here if needed
+  }
+
+  fetchImages = async () => {
+    const { query, page } = this.state;
+    const url = `https://pixabay.com/api/?key=${API_KEY}&q=${query}&page=${page}&image_type=photo&per_page=12`;
+
+    this.setState({ isLoading: true });
+
+    try {
+      const response = await axios.get(url);
+      const newImages = response.data.hits;
+
+      this.setState(prevState => ({
+        images: [...prevState.images, ...newImages],
+        isLoading: false,
+      }));
+    } catch (error) {
+      this.setState({ error: error.message, isLoading: false });
+    }
+  };
+
+  handleSubmit = query => {
+    this.setState({ query, images: [], page: 1 });
+  };
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  handleImageClick = imageUrl => {
+    this.setState({
+      selectedImage: imageUrl,
+      showModal: true,
+    });
+  };
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false, selectedImage: null });
+  };
+
+  render() {
+    const { images, isLoading, error, showModal, selectedImage } = this.state;
+
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleSubmit} />
+        {error && <p>Error: {error}</p>}
+        <ImageGallery images={images} onImageClick={this.handleImageClick} />
+        {isLoading && <Loader />}
+        {images.length > 0 && !isLoading && (
+          <Button onClick={this.handleLoadMore} />
+        )}
+        {showModal && (
+          <Modal
+            imageUrl={selectedImage}
+            onCloseModal={this.handleCloseModal}
+          />
+        )}
+      </div>
+    );
+  }
+}
